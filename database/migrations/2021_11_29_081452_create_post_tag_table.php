e80731c73146ec2cbaa05f0e296c818d54a8e9c8
marbou-com <livefordreams.rev@gmail.com>
2021-12-27 17:17:31 +0900

    aaa

--- database/migrations/2021_11_29_081452_create_post_tag_table.php
+++ database/migrations/2021_11_29_081452_create_post_tag_table.php
@@ -15,8 +15,8 @@ class CreatePostTagTable extends Migration
     {
         Schema::create('post_tag', function (Blueprint $table) {
             $table->id();
-            $table->unsignedInteger('tag_id'); //この行を追加
-            $table->unsignedInteger('post_id'); //この行を追加
+            $table->unsignedBigInteger('tag_id'); //この行を追加
+            $table->unsignedBigInteger('post_id'); //この行を追加
             $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade'); //この行を追加
             $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
             $table->timestamps();
